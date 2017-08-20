2f5195ae9320feb6fe3bdd777dba859f390fa64f
Alexandre Guevara <AlexGuevara@Alexandres-MacBook-Pro.local>
2017-08-20 04:16:21 -0700

    work on BenefitsPage functionality

--- client/components/BenefitsList.js
+++ client/components/BenefitsList.js
@@ -3,45 +3,40 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/fetchBenefits';
-import Salary from './Salary';
+import $ from 'jquery';
 
 class BenefitsList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { salary: '65000'};
+    // console.log($(this)[0].state.salary)
+    // console.log("test");
+
+
   }
-
-//   commafy(num) {
-//     var str = num.toString().split('.');
-//     if (str[0].length >= 5) {
-//         str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
-//     }
-//     if (str[1] && str[1].length >= 5) {
-//         str[1] = str[1].replace(/(\d{3})/g, '$1 ');
-//     }
-//     return str.join('.');
-// }
-
 
 
   renderBenefits() {
-    console.log(this.props);
+
+
     return this.props.data.benefits.map(({ id, name, cost }) => {
-      // const benefitCost = commafy(cost);
       return (
-        <div key={id} className="benefits-card">
+
+        <div key={id} id={id} className="benefits-card" onClick={() =>
+
+          $('#'+{id}.id).toggleClass("deselected")
+            // .then(
+            //   this.setState({ salary: this.state.salary - ({cost}.cost)*-1})
+            // )
+            // .then(console.log({cost}.cost))
+
+        }>
           <div className="benefits-image"></div>
           <div className="benefits-name">{name}</div>
           <div className="benefits-description">This will be a description for each benefit</div>
           <div className="benefits-cost">${cost}</div>
         </div>
-
-
-
-
-        // <li key={id} className="collection-item">
-        //   {name}
-        //   {cost}
-        // </li>
       );
     });
   }
@@ -49,16 +44,23 @@ class BenefitsList extends Component {
   render() {
     if(this.props.data.loading) {return <div /> }
 
+    // if($('.benefits-card').hasClass('deselected')){
+    //   console.log("this is deselected");
+    // }
+
+    if($('.benefits-card').hasClass("deselected")){
+      alert("deselected");
+     this.setState({ salary: this.state.salary - ({cost}.cost)*-1})
+   }
+
     return (
       <div className="benefits-container">
-        <Salary />
+        <div className="net-salary">
+          <div className="salary-net">${this.state.salary}</div>
+          <div className="salary-text">Total Compensation</div>
+        </div>
         {this.renderBenefits()}
       </div>
-      // <div>
-      //   <ul className="collection">
-      //     {this.renderBenefits()}
-      //   </ul>
-      // </div>
 
     )
   }
--- client/components/Salary.js
+++ client/components/Salary.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 class NetSalary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { salary: 65000};
+  }
+
   render() {
     return (
 
       <div className="net-salary">
-        <div className="salary-net">$65,000</div>
+        <div className="salary-net">${this.state.salary}</div>
         <div className="salary-text">Total Compensation</div>
       </div>
     )
